e55e39adb57a73e7b03f12e53caa665333cc0cde
Abir Basumatary <abirbasumatary11@gmail.com>
2022-08-15 11:00:06 +0530

    first commit

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const d = new Date()
+let year = d.getFullYear()
+function Footer() {
+  return <footer>
+    <p>Copyright {year}</p>
+  </footer>
+}
+export default Footer;
